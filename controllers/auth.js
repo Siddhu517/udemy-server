6458bcc796de2a9ed1530ee798d9f79e3d4a3917
Siddhu517 <siddhusadadekar517@gmail.com>
2022-05-11 11:08:08 +0530

    ready to deploy

---
+++ controllers/auth.js
@@ -0,0 +1,289 @@
+import { nanoid } from "nanoid";
+import jwt from "jsonwebtoken";
+import User from "../models/user";
+import { hashPassword, comparePassword } from "../helpers/auth";
+
+export const register = async (req, res) => {
+  const { name, email, password, secret } = req.body;
+  // validation
+  if (!name) {
+    return res.json({
+      error: "Name is required",
+    });
+  }
+  if (!password || password.length < 6) {
+    return res.json({
+      error: "Password is required and should be min 6 characters long",
+    });
+  }
+  if (!secret) {
+    return res.json({
+      error: "Answer is required",
+    });
+  }
+  const exist = await User.findOne({ email });
+  if (exist) {
+    return res.json({
+      error: "Email is taken",
+    });
+  }
+
+  // hash password
+  const hashedPassword = await hashPassword(password);
+
+  const user = new User({
+    name,
+    email,
+    password: hashedPassword,
+    secret,
+    username: nanoid(6), // eg. nanoid(10) //=> "IRFa-VaY2b"
+  });
+  try {
+    await user.save();
+    // return res.json(user);
+    return res.json({
+      ok: true,
+    });
+  } catch (err) {
+    console.log("CREATE FAILED", err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+export const login = async (req, res) => {
+  try {
+    // console.log(req.body);
+    const { email, password } = req.body;
+    // check if our db has user with that email
+    const user = await User.findOne({ email }).exec();
+    if (!user) {
+      return res.json({
+        error: "No user found",
+      });
+    }
+
+    // check password
+    const match = await comparePassword(password, user.password);
+    if (!match) {
+      return res.json({
+        error: "Wrong password",
+      });
+    }
+
+    // create signed token
+    const token = jwt.sign({ _id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "7d", // 20 * 60 = 20 sec
+    });
+    // return user and token to client, exclude password and secret
+    user.password = undefined;
+    user.secret = undefined;
+
+    res.json({
+      token,
+      user,
+    });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).send("Error. Try again.");
+  }
+};
+
+export const currentUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    res.json({ ok: true });
+  } catch (err) {
+    console.log(err);
+    res.sendStatus(400);
+  }
+};
+
+export const forgotPassword = async (req, res, next) => {
+  console.log(req.body);
+  const { email, newPassword, secret } = req.body;
+  // validation
+  if (!newPassword || !newPassword.length < 6) {
+    return res.json({
+      error: "New password is required and should be min 6 characters long",
+    });
+  }
+  if (!secret) {
+    return res.json({
+      error: "Secret is required",
+    });
+  }
+  let user = await User.findOne({ email, secret });
+  // console.log("EXIST ----->", user);
+  if (!user) {
+    return res.json({
+      error: "We cant verify you with those details",
+    });
+  }
+  // return res.status(400).send("We cant verify you with those details");
+
+  try {
+    const hashed = await hashPassword(newPassword);
+    await User.findByIdAndUpdate(user._id, { password: hashed });
+    return res.json({
+      success: "Congrats. Now you can login with your new password",
+    });
+  } catch (err) {
+    console.log(err);
+    return res.json({
+      error: "Something wrong. Try again.",
+    });
+  }
+};
+
+export const profileUpdate = async (req, res) => {
+  try {
+    // console.log("profile update req", req.body);
+    const data = {};
+
+    if (req.body.username) {
+      data.username = req.body.username;
+    }
+    if (req.body.about) {
+      data.about = req.body.about;
+    }
+    if (req.body.name) {
+      data.name = req.body.name;
+    }
+    if (req.body.password) {
+      if (req.body.password.length < 6) {
+        return res.json({
+          error: "Password is required and should be min 6 characters long",
+        });
+      } else {
+        data.password = await hashPassword(req.body.password);
+      }
+    }
+    if (req.body.secret) {
+      data.secret = req.body.secret;
+    }
+    if (req.body.image) {
+      data.image = req.body.image;
+    }
+
+    let user = await User.findByIdAndUpdate(req.user._id, data, { new: true });
+    // console.log('updated user', user)
+    user.password = undefined;
+    user.secret = undefined;
+    res.json(user);
+  } catch (err) {
+    if (err.code == 11000) {
+      return res.json({ error: "Duplicate username" });
+    }
+    console.log(err);
+  }
+};
+
+export const findPeople = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    //user.following
+    let following = user.following;
+    following.push(req.user._id);
+    const people = await User.find({ _id: { $nin: following } })
+      .select("-password -secret")
+      .limit(10);
+    res.json(people);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// addFollower, userFollow
+//middleware
+export const addFollower = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.body._id, {
+      $addToSet: { followers: req.user._id },
+    });
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const userFollow = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $addToSet: { following: req.body._id },
+      },
+      { new: true }
+    ).select("-password -secret");
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const userFollowing = async (req, res) => {
+  try {
+    const user = await User.findById(req.user._id);
+    const following = await User.find({ _id: user.following }).limit(100);
+    res.json(following);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+// removeFollower, userUnfollow
+// middleware
+export const removeFollower = async (req, res, next) => {
+  try {
+    const user = await User.findByIdAndUpdate(req.body._id, {
+      $pull: { followers: req.user._id },
+    });
+    next();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const userUnfollow = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        $pull: { following: req.body._id },
+      },
+      { new: true }
+    );
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const searchUser = async (req, res) => {
+  const { query } = req.params;
+  if (!query) return;
+  try {
+    //$regex is special method from mongodb
+    //the i modifier is used to perform case-insensitive matching.
+    const user = await User.find({
+      $or: [
+        { name: { $regex: query, $options: "i" } },
+        { username: { $regex: query, $options: "i" } },
+      ],
+    }).select("-password -secret");
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const getUser = async (req, res) => {
+  try {
+    const user = await User.findOne({ username: req.params.username }).select(
+      "-password -secret"
+    );
+    res.json(user);
+  } catch (err) {
+    console.log(err);
+  }
+};
